787a2967068e707abbf6b52762ff7c89dc147ca6
euphory <joelvallejo17@gmail.com>
2020-12-13 14:44:27 +0100

    Initial commit

---
+++ src/main/java/com/CabinetVeterinaireApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetVeterinaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinetVeterinaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kindsonthegeniuspro/medicalab/MedicalabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kindsonthegeniuspro.medicalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
